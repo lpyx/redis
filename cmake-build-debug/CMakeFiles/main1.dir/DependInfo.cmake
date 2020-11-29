9aeca1876f4968dea29080bfb8350069566e6a7c
吴晓刚 <wuxiaogang@wuxiaogangdeMacBook-Pro.local>
2020-11-29 22:35:27 +0800

    增加调试

---
+++ cmake-build-debug/CMakeFiles/main1.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/wuxiaogang/clion/redis/test.c" "/Users/wuxiaogang/clion/redis/cmake-build-debug/CMakeFiles/main1.dir/test.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../deps/hiredis"
+  "../deps/hiredis/adapters"
+  "../deps/hiredis/examples"
+  "../deps/jemalloc/include"
+  "../deps/jemalloc/include/jemalloc"
+  "../deps/jemalloc/include/jemalloc/internal"
+  "../deps/jemalloc/include/msvc_compat"
+  "../deps/jemalloc/include/msvc_compat/C99"
+  "../deps/jemalloc/test/include"
+  "../deps/jemalloc/test/include/test"
+  "../deps/linenoise"
+  "../deps/lua/etc"
+  "../deps/lua/src"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ test.c
@@ -0,0 +1,10 @@
+//
+// Created by 吴晓刚 on 2020/11/29.
+//
+
+#include <stdio.h>
+
+int main(){
+    printf("hello world\n");
+    return 0;
+}
